aeb743d5f47b3c1a12ca29027366aace93021b31
Edy Segura <edysegura@gmail.com>
2022-10-08 14:13:35 -0300

    feat: implement activate lifecycle

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@ self.addEventListener('install', () => {
   console.log('[Service Worker] Installing service worker');
   self.skipWaiting();
 });
+
+self.addEventListener('activate', () => {
+  console.log('[Service Worker] Activating service worker!');
+  return self.clients.claim();
+});
