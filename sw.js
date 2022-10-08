c7a49e9983c7f78ec8f0011c2c4167ef4cb18db3
Edy Segura <edysegura@gmail.com>
2022-10-08 14:48:44 -0300

    feat: control browser requests with sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-self.addEventListener('install', () => {
-  console.log('[Service Worker] Installing service worker');
+self.addEventListener("install", () => {
+  console.log("[Service Worker] Installing service worker");
   self.skipWaiting();
 });
 
-self.addEventListener('activate', () => {
-  console.log('[Service Worker] Activating service worker!');
+self.addEventListener("activate", () => {
+  console.log("[Service Worker] Activating service worker!");
   return self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("[Service Worker] Fetch event worker!", event.request.url);
+  event.respondWith(fetch(event.request.url));
 });
